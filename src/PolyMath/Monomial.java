ebd6bd1b6dd1521f16d8e5f747658de0e0d33ad6
Eldar <eldarzr@post.bgu.ac.il>
2021-04-27 20:00:19 +0300

    a

--- src/PolyMath/Monomial.java
+++ src/PolyMath/Monomial.java
@@ -1,4 +1,24 @@
 package PolyMath;
 
-public class Monomial {
+public class Monomial implements Nomial<Monomial> {
+
+    @Override
+    public Monomial add(Monomial monomial) {
+        return null;
+    }
+
+    @Override
+    public Monomial mult(Monomial monomial) {
+        return null;
+    }
+
+    @Override
+    public Scalar evaluate(Scalar s) {
+        return null;
+    }
+
+    @Override
+    public Monomial derivative() {
+        return null;
+    }
 }
